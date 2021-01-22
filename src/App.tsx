8ed68856589dd824b8ccc7a4296099e9be6c84a8
Cinnamon <cinnamon_bun_github@fastmail.com>
2021-01-22 13:41:59 -0800

    store react-earthstar state in localStorage.  also minor code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,121 @@
 import React from "react";
 import "./App.css";
-import { isErr, Document } from "earthstar";
+import { isErr, Document, AuthorKeypair, StorageMemory, ValidatorEs4 } from "earthstar";
 import {
-  EarthstarPeer,
-  Earthbar,
-  MultiWorkspaceTab,
-  AuthorTab,
-  Spacer,
-  useWorkspaces,
-  useDocument,
-  useCurrentAuthor,
-  WorkspaceLabel,
-  useDocuments,
   AuthorLabel,
+  AuthorTab,
+  Earthbar,
+  EarthstarPeer,
+  MultiWorkspaceTab,
+  Spacer,
+  WorkspaceLabel,
+  useCurrentAuthor,
+  useCurrentWorkspace,
+  useDocument,
+  useDocuments,
+  usePubs,
   useStorages,
+  useSubscribeToStorages,
+  useWorkspaces,
 } from "react-earthstar";
 import { formatDistance, differenceInSeconds } from "date-fns";
 import "react-earthstar/styles/layout.css";
 
+import { useLocalStorage, writeStorage } from "@rehooks/local-storage";
+
+//================================================================================
+// LOCALSTORAGE PERSISTENCE FOR REACT-EARTHSTAR
+
+const LS_AUTHOR_KEY = "earthstar-status-currentAuthor";
+const LS_PUBS_KEY = "earthstar-status-pubs";
+const LS_STORAGES_DOCS_KEY = "earthstar-status-storages-docs";
+const LS_CURRENT_WORKSPACE_KEY = "earthstar-status-current-workspace";
+
+// This is a deeply nested object with keys like:
+//   workspaceAddress:
+//      path:
+//        author:
+//            Document
+type WorkspaceRecords = Record<string, Record<string, Record<string, Document>>>;
+// workspaceAddress -> list of pub URLs
+type PubRecords = Record<string, string[]>;
+
+// this saves the state of react-earthstar to localStorage
+function Persistor() {
+  const [storages] = useStorages();
+  const [pubs] = usePubs();
+  const [currentAuthor] = useCurrentAuthor();
+  const [currentWorkspace] = useCurrentWorkspace();
+
+  useSubscribeToStorages({
+    onWrite: (event) => {
+      const storage = storages[event.document.workspace];
+      writeStorage(LS_STORAGES_DOCS_KEY, {
+        ...storages,
+        [event.document.workspace]: (storage as StorageMemory)._docs,
+      });
+    },
+  });
+
+  React.useEffect(() => {
+    Object.values(storages).forEach((storage) => {
+      writeStorage(LS_STORAGES_DOCS_KEY, {
+        ...storages,
+        [storage.workspace]: (storage as StorageMemory)._docs,
+      });
+    });
+  }, [storages]);
+
+  React.useEffect(() => {
+    writeStorage(LS_PUBS_KEY, pubs);
+  }, [pubs]);
+
+  React.useEffect(() => {
+    writeStorage(LS_AUTHOR_KEY, currentAuthor);
+  }, [currentAuthor]);
+
+  React.useEffect(() => {
+    writeStorage(LS_CURRENT_WORKSPACE_KEY, currentWorkspace);
+  }, [currentWorkspace]);
+
+  return null;
+}
+
+//================================================================================
+// MAIN APP COMPONENT
+
 function App() {
+  // load the initial state from localStorage
+  const [workspacesDocsInStorage] = useLocalStorage<WorkspaceRecords>(LS_STORAGES_DOCS_KEY, {});
+  const [pubsInStorage] = useLocalStorage<PubRecords>(LS_PUBS_KEY, {});
+  const [currentAuthorInStorage] = useLocalStorage<AuthorKeypair>(LS_AUTHOR_KEY);
+  const [currentWorkspaceInStorage] = useLocalStorage(LS_CURRENT_WORKSPACE_KEY);
+
+  const initWorkspaces = Object.entries(workspacesDocsInStorage).map(
+    ([workspaceAddress, docs]) => {
+      const storage = new StorageMemory([ValidatorEs4], workspaceAddress);
+      // (this is a hack that knows too much about the internal structure of StorageMemory)
+      // (it would be better to ingest each document one by one, but also a lot slower)
+      storage._docs = docs;
+      return storage;
+    }
+  );
+
   return (
     <div className="App">
-      <EarthstarPeer>
+      <EarthstarPeer
+        initPubs={pubsInStorage}
+        initWorkspaces={initWorkspaces}
+        initCurrentAuthor={currentAuthorInStorage}
+        initCurrentWorkspace={currentWorkspaceInStorage}
+      >
         <Earthbar>
           <MultiWorkspaceTab />
           <Spacer />
           <AuthorTab />
         </Earthbar>
-        <OnlineWriter />
+        <Persistor />
+        <OnlineHeartbeatWriter />
         <StatusPoster />
         <StatusesList />
       </EarthstarPeer>
@@ -35,13 +123,8 @@ function App() {
   );
 }
 
-// Cinnamon
-// - +gardening
-//   - my basil is flowering!
-// - +earthstardev
-//   - I just wrote some great docs
-
-function OnlineWriter() {
+// this saves a heartbeat document every N seconds to know if you're online or not
+function OnlineHeartbeatWriter() {
   const [storages] = useStorages();
   const [currentAuthor] = useCurrentAuthor();
 
@@ -59,7 +142,7 @@ function OnlineWriter() {
         });
       });
 
-      console.log("<3");
+      console.log("heartbeat", Date.now());
     }, 20000);
 
     return () => {
@@ -70,6 +153,8 @@ function OnlineWriter() {
   return null;
 }
 
+// an input for writing your status.
+// also has a dropdown to choose which workspace to save it in.
 function StatusPoster() {
   const workspaces = useWorkspaces();
 
@@ -127,6 +212,7 @@ function StatusPoster() {
   );
 }
 
+// show all the statuses for all the workspaces
 function StatusesList() {
   const workspaces = useWorkspaces();
 
@@ -139,6 +225,8 @@ function StatusesList() {
   );
 }
 
+
+// show all the statuses in one workspace
 type WorkspaceStatusesProps = {
   address: string;
 };
@@ -168,6 +256,24 @@ function WorkspaceStatuses({
   );
 }
 
+
+// compute an "oldness" css class for a date
+type Oldness = "recent" | "old" | "ancient";
+function howOld(date: Date): Oldness {
+  const daysOld = (Date.now() - date.getTime()) / 1000 / 60 / 60 / 24;
+
+  if (daysOld > 365) {
+    return "ancient";
+  }
+
+  if (daysOld > 30) {
+    return "old";
+  }
+
+  return "recent";
+}
+
+// show a single person's status
 type StatusProps = {
   doc: Document;
 };
@@ -209,22 +315,7 @@ function Status({ doc }: StatusProps) {
   );
 }
 
-type Oldness = "recent" | "old" | "ancient";
-
-function howOld(date: Date): Oldness {
-  const daysOld = (Date.now() - date.getTime()) / 1000 / 60 / 60 / 24;
-
-  if (daysOld > 365) {
-    return "ancient";
-  }
-
-  if (daysOld > 30) {
-    return "old";
-  }
-
-  return "recent";
-}
-
+// an icon that shows if the person is recently online
 type OnlineIndicatorProps = {
   authorAddress: string;
   workspaceAddress: string;
